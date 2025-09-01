25be9ea65aee55c839a9162adc08c54dcd7434e2
CarolayDS <carolaydelgadosantiago@gmail.com>
2025-09-01 15:57:42 -0500

    update hero- slogan

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,7 +2,7 @@
 import mainPhoto3 from "../assets/images/hero3.svg";
 import { themeColor } from "../data/items";
 
-const heroHeading = "Liberando horas, mejorando clases";
+const heroHeading = "Ayudamos a profesores y transformamos la educación";
 const buttonSeeFeature = "Ver Características";
 const buttonPricing = "Precio";
 </script>
